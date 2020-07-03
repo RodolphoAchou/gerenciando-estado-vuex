70ff58411a61283c68e9763b34c400320a787c8a
rodolphoachou <rodolphodealmeida@hotmail.com>
2020-07-03 09:18:54 -0300

    Criando contador

---
+++ src/components/Counter.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h1>Counter: {{ counter }}</h1>
+        <button @click="decrement" >Decrement</button>
+        <button @click="increment" >Increment</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Counter',
+    data: () => ({
+        counter: 0
+    }),
+    methods: {
+        decrement(){
+            this.counter --
+        },
+        increment(){
+            this.counter ++
+        },
+    }
+}
+</script>
